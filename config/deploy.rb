8127d51c8e9bb66234e9ce5c93129f483b27fe2e
seba <sebaryco@gmail.com>
2012-10-12 21:39:05 -0300

    ok

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+set :application, "Feriados"
+set :repository,  "git@github.com:sebaxtian08/Feriados.git"
+
+set :scm, :subversion
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, "git"
+set :user, "sebaryco@gmail.com"  # The server's user for deploys
+set :scm_passphrase, "heroku2012"  # The deploy user's password
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
